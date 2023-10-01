a59f595d251649e053fb3f71f34b86862426ff77
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-01 18:59:18 +0530

    Create sysinfo.py

---
+++ main/plugins/sysinfo.py
@@ -0,0 +1,28 @@
+from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+import psutil, os, signal
+
+Drone.on(events.NewMessage(incoming=True, pattern="/sysinfo"))
+async def sysinfo(event):
+    try:
+        zyl = "neofetch --stdout"
+        fetch = await asyncrunapp(
+            zyl,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+        stdout, stderr = await fetch.communicate()
+        result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+        await event.reply("**" + result + "**")
+    except FileNotFoundError:
+        await event.reply("**Install neofetch first**")
+
+
+async def info(file, event):
+    process = subprocess.Popen(
+        ["mediainfo", file, "--Output=HTML"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
