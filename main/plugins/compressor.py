f342cf68db3b7274a1ff1bf5daf8f533258941ab
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-01 18:57:14 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -45,7 +45,7 @@ async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
         out = new_name + ".mp4"
     elif 'x-matroska' in mime:
         n = "media_" + dt.now().isoformat("_", "seconds") + ".mp4" 
-        out = new_name + ".mp4"            
+        out = new_name + ".mkv"            
     elif 'webm' in mime:
         n = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
         out = new_name + ".mp4"
@@ -75,7 +75,7 @@ async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
     progress = f"progress-{FT}.txt"
     cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" None """{out}""" -y'
     if ffmpeg_cmd == 1:
-        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -crf 22 -acodec copy -c:s copy """{out}""" -y'
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -crf 23 -acodec copy -c:s copy """{out}""" -y'
     elif ffmpeg_cmd == 2:
         cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -c:v libx265 -crf 22 -preset ultrafast -s 640x360 -c:a copy -c:s copy """{out}""" -y'
     elif ffmpeg_cmd == 3:
