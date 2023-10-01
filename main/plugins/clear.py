4ddbc3532628333acae5947ddce09c59792d417d
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-01 19:59:12 +0530

    Create clear.py

---
+++ main/plugins/clear.py
@@ -0,0 +1,21 @@
+from .. import Drone
+from telethon import events, Button
+from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+import psutil, os, signal
+
+@Drone.on(events.NewMessage(incoming=True, pattern="/ping"))
+async def test(event):
+    try:
+        zylern = "rm -r *.mp4"
+        fetch = await asyncrunapp(
+            zylern,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+        stdout, stderr = await fetch.communicate()
+        result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+        await event.reply("**" + result + "**")
+    except FileNotFoundError:
+        await event.reply("**Unable to Clear System Files**")
