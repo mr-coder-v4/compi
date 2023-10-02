da93da9cae31c2b7ed8c8de98cd2148e848970fd
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-02 11:56:35 +0530

    Update memory2.py

--- main/plugins/memory2.py
+++ main/plugins/memory2.py
@@ -3,6 +3,7 @@ from telethon import events, Button
 from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 import psutil, os, signal, sys, platform, sysconfig
+memory = psutil.virtual_memory().total / (1024.0 ** 3)
 from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/memory2"))
