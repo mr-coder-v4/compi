9c3d94ee29e234efbd834e18a6a8157da4ebacc3
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-02 11:59:57 +0530

    Update memory2.py

--- main/plugins/memory2.py
+++ main/plugins/memory2.py
@@ -3,18 +3,17 @@ from telethon import events, Button
 from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 import psutil, os, signal, sys, platform, sysconfig
-memory = psutil.virtual_memory().total / (1024.0 ** 3)
 from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/memory2"))
 async def mem2(event):
     total, used, free, disk= disk_usage('/')
-    total = str(total)
-    free = str(free)
-    memory = virtual_memory()
+    total = str(total.total / (1024.0 ** 3))
+    free = str(free.total / (1024.0 ** 3))
+    memory = virtual_memory().total / (1024.0 ** 3)
     mem_p = str(memory.percent)
-    mem_t = str(memory.total)
-    mem_a = str(memory.available)
-    mem_u = str(memory.used)
+    mem_t = str(memory.total / (1024.0 ** 3))
+    mem_a = str(memory.available / (1024.0 ** 3))
+    mem_u = str(memory.used / (1024.0 ** 3))
     await event.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\n**Total Disk Space:** {total}\n**Free:** {free}\n**Memory Total:** {mem_t}\n**Memory Free:** {mem_a}\n**Memory Used:** {mem_u}\n")
     return
