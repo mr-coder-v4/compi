f45a94a49ca2418d073a7bc9251382088a044236
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-02 13:40:00 +0530

    Update storage.py

--- main/plugins/storage.py
+++ main/plugins/storage.py
@@ -12,8 +12,8 @@ async def storage(event):
     free = round(free/1024.0/1024.0/1024.0,1)
     memory = virtual_memory()
     mem_p = round(memory.percent)
-    mem_t = round(memory.total/1024.0/1024.0/1024.0,1)
-    mem_a = round(memory.available/1024.0/1024.0/1024.0,1)
-    mem_u = round(memory.used/1024.0/1024.0/1024.0,1)
+    mem_t = round(memory.total/1024.0/1024.0,1)
+    mem_a = round(memory.available/1024.0/1024.0,1)
+    mem_u = round(memory.used/1024.0/1024.0,1)
     await event.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\n**Total Disk Space:** {total}\n**Free:** {free}\n**Memory Percent:** {mem_p}\n**Memory Total:** {mem_t}\n**Memory Free:** {mem_a}\n**Memory Used:** {mem_u}\n")
     return
