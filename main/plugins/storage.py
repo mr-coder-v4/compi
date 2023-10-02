cac75825e16e7e117754a9e19f0b497514b58e0e
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-02 12:29:31 +0530

    Update storage.py

--- main/plugins/storage.py
+++ main/plugins/storage.py
@@ -8,12 +8,12 @@ from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
 @Drone.on(events.NewMessage(incoming=True, pattern="/storage"))
 async def storage(event):
     total, used, free, disk= disk_usage('/')
-    total = str(total / (1024.0 ** 3))
-    free = str(free / (1024.0 ** 3))
+    total = str(total / (1024^3))
+    free = str(free / (1024.0 ** 2))
     memory = virtual_memory()
     mem_p = str(memory.percent)
-    mem_t = str(memory.total / (1024.0 ** 3))
-    mem_a = str(memory.available / (1024.0 ** 3))
+    mem_t = str(memory.total / (1024.0 ** 2))
+    mem_a = str(memory.available / (1024.0 ** ))
     mem_u = str(memory.used / (1024.0 ** 3))
     await event.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\n**Total Disk Space:** {total}\n**Free:** {free}\n**Memory Total:** {mem_t}\n**Memory Free:** {mem_a}\n**Memory Used:** {mem_u}\n")
     return
