e6f804ede3bf24468cb6ed3d7214c926bd5ea0f3
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-03 19:58:09 +0530

    Update dbstuff.py

--- main/plugins/dbstuff.py
+++ main/plugins/dbstuff.py
@@ -39,8 +39,7 @@ async def listusers(event):
 @Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="/list"))
 async def user(event):
     all_users = await db.get_users()
-    async for all_users:
-        user_id = user.get("id") 
+    user_id = all_users.get("id") 
     await event.reply(f"List of Total user(s) (user_id)")
     
 @Drone.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="/bcast"))
