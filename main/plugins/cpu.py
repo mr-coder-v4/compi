285d8d4a773a165c77f45562b38c7db322a0d6ff
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-03 14:52:12 +0530

    Update cpu.py

--- main/plugins/cpu.py
+++ main/plugins/cpu.py
@@ -10,7 +10,8 @@ async def storage(event):
     cpu = psutil.cpu_percent(interval=1)
     cpu_usage = [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
     cpu_total = psutil.cpu_count()
+    cpu_logical = psutil.cpu_count(logical=False)
     cpu_physical = psutil.cpu_count(logical=False)
     cpu_usable = len(psutil.Process().cpu_affinity())
-    await event.reply(f"**OS: {platform.system()}**\n**Version: {platform.release()}**\n**Architecture: {platform.architecture()}**\nCPU Utilization: {cpu}%\nCPU Load Past 1, 5, 15 Min: {cpu_usage}\nTotal CPU Cores: {cpu_total}\nUsable CPU Cores: {cpu_usable}\nCPU Physical Cores: {cpu_physical}\n")
+    await event.reply(f"**OS: {platform.system()}**\n**Version: {platform.release()}**\n**Architecture: {platform.architecture()}**\nCPU Utilization: {cpu}%\nCPU Load Past 1, 5, 15 Min: {cpu_usage}\nTotal CPU Cores: {cpu_total}\nUsable CPU Cores: {cpu_usable}\nLogical CPU Cores: {cpu_logical}\nPhysical CPU Cores: {cpu_physical}\n")
     return
