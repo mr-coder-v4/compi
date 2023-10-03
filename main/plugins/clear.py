4c115b0976e45aa828c16bb2bb20c29bb9801faa
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-03 16:48:40 +0530

    Add files via upload

---
+++ main/plugins/clear.py
@@ -0,0 +1,21 @@
+from .. import Drone
+from telethon import events, Button
+from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+import psutil, os, signal
+
+@Drone.on(events.NewMessage(incoming=True, pattern="/clear"))
+async def clear(event):
+    try:
+        zylern = "rm -r *.mp4"
+        fetch = await asyncrunapp(
+            zylern,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+        stdout, stderr = await fetch.communicate()
+        result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+        await event.reply("**" + result + "**")
+    except FileNotFoundError:
+        await event.reply("**Install speedtest-cli**")
