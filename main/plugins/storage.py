55d1d63070e993dcec8ec3a1b3c898126be10506
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-03 14:06:20 +0530

    Update storage.py

--- main/plugins/storage.py
+++ main/plugins/storage.py
@@ -8,8 +8,8 @@ from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
 @Drone.on(events.NewMessage(incoming=True, pattern="/storage"))
 async def storage(event):
     disk= psutil.disk_usage('/')
-    d_tot = round(disk.total/1024.0/1024.0/1024.0,1)
-    d_use = round(disk.used/1024.0/1024.0/1024.0,1)
+    d_t = round(disk.total/1024.0/1024.0/1024.0,1)
+    d_u = round(disk.used/1024.0/1024.0/1024.0,1)
     d_p = disk.percent
     d_f = round(disk.free/1024.0/1024.0/1024.0,1)
     memory = psutil.virtual_memory()
@@ -19,5 +19,5 @@ async def storage(event):
     mem_a = round(memory.available/1024.0/1024.0/1024.0,1)
     mem_f = round(memory.free/1024.0/1024.0/1024.0,1)
     mem_u = round(memory.used/1024.0/1024.0/1024.0,1)
-    await event.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\n---------------------------------\nTotal Disk Space: {d_tot} GB\nUsed Disk Space: {d_use} GB\nUsed Disk Percentage: {d_f}%\nAvailable Disk Space: {d_p} GB\n---------------------------------\nMemory Utilization: {mem_p}%\nTotal RAM: {mem_t} GB\nAvailable RAM: {mem_a} GB\nFree RAM: {mem_f} GB\nRAM Utilized: {mem_u} GB\n")
+    await event.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\n---------------------------------\nTotal Disk Space: {d_t} GB\nUsed Disk Space: {d_u} GB\nUsed Disk Percentage: {d_p}%\nAvailable Disk Space: {d_f} GB\n---------------------------------\nMemory Utilization: {mem_p}%\nTotal RAM: {mem_t} GB\nAvailable RAM: {mem_a} GB\nFree RAM: {mem_f} GB\nRAM Utilized: {mem_u} GB\n")
     return
