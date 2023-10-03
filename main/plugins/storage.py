1c2e6f42ad317f057d18b2ffc198af110e868f0a
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-03 12:17:28 +0530

    Update storage.py

--- main/plugins/storage.py
+++ main/plugins/storage.py
@@ -15,8 +15,5 @@ async def storage(event):
     mem_t = round(memory.total/1024.0/1024.0/1024.0,1)
     mem_a = round(memory.available/1024.0/1024.0/1024.0,1)
     mem_u = round(memory.used/1024.0/1024.0/1024.0,1)
-    cpu = str(psutil.cpu_percent(5))
-    load1, load5, load10 = psutil.getloadavg()
-    cpu_usage = (load10/os.cpu_count()) * 100
-    await event.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\nCPU Utilization: {cpu}%\nCPU Load Past 10 Min: {cpu_usage}\nTotal Disk Space: {total} GB\nAvailable Disk Space: {free} GB\nMemory Utilization: {mem_p}%\nTotal RAM: {mem_t} GB\nAvailable RAM: {mem_a} GB\n RAM Utilized: {mem_u} GB\n")
+    await event.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\nTotal Disk Space: {total} GB\nAvailable Disk Space: {free} GB\nMemory Utilization: {mem_p}%\nTotal RAM: {mem_t} GB\nAvailable RAM: {mem_a} GB\n RAM Utilized: {mem_u} GB\n")
     return
