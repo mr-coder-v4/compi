526485692b9e09f20c45a83158df8a85d3b7c36e
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-04 18:04:13 +0530

    Update quick_info.py

--- main/plugins/quick_info.py
+++ main/plugins/quick_info.py
@@ -9,10 +9,10 @@ async def quick_info(event):
     memory = psutil.virtual_memory()
     available = round(memory.available/1024.0/1024.0/1024.0,2)
     total = round(memory.total/1024.0/1024.0/1024.0,2)
-    mem_info = str(available) + 'GB' + str(memory.percent) + '%' / + str(total) + 'GB'
+    mem_info = str(available) '+ GB +' str(memory.percent) '+ % /' + str(total) '+ GB'
     disk = psutil.disk_usage('/')
     free = round(disk.free/1024.0/1024.0/1024.0,2)
     total = round(disk.total/1024.0/1024.0/1024.0,2)
-    disk_info = str(free) + 'GB' + str(disk.percent) + '%'/ + str(total) + 'GB'
+    disk_info = str(free) '+ GB' + str(disk.percent) + '%/' + str(total) '+ GB'
     await event.reply(f"CPU: {cpu}\nDisk: {disk_info}\nMemory: {mem_info}")
     return
