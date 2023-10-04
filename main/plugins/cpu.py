311e470a95301025779a112d95e39df11631b676
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-04 13:33:41 +0530

    Update cpu.py

--- main/plugins/cpu.py
+++ main/plugins/cpu.py
@@ -2,13 +2,13 @@ from .. import Drone
 from telethon import events, Button
 from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
-import psutil, os, signal, sys, platform, sysconfig
+import psutil, os, signal, sys, platform, sysconfig, multiprocessing
 from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/cpu"))
 async def storage(event):
     cpu = psutil.cpu_percent(interval=1)
-    cpu10 = psutil.cpu_percent(interval=10)
+    cpu_o = multiprocessing.cpu_count()
     load1, load5, load15 = psutil.getloadavg()
     cpu_usage1 = (load1/os.cpu_count()) * 100
     cpu_usage5 = (load5/os.cpu_count()) * 100
@@ -20,5 +20,5 @@ async def storage(event):
     cpu_cur = freq.current
     cpu_max = freq.max
     cpu_min = freq.min
-    await event.reply(f"**OS: {platform.system()}**\n**Version: {platform.release()}**\n**Architecture: {platform.architecture()}**\n-----------------------------------------------------------------\nCPU Utilization: {cpu}%\nCPU Utilized Past 10sec: {cpu10}%\nTotal CPU Cores: {cpu_total}\nUsable CPU Cores: {cpu_usable}\nPhysical CPU Cores: {cpu_physical}\n-----------------------------------------------------------------\nCPU Frequency :-\nCurrent: {cpu_cur} mhz\nMAX: {cpu_max} mhz\nMIN: {cpu_min} mhz\n-----------------------------------------------------------------\nLoad Past 1 Min: {cpu_usage1}\nLoad Past 5 Min: {cpu_usage5}\nLoad Past 15 Min: {cpu_usage15}\n")
+    await event.reply(f"**OS: {platform.system()}**\n**Version: {platform.release()}**\n**Architecture: {platform.architecture()}**\n-----------------------------------------------------------------\nCPU Utilization: {cpu}%\nCPU Cores Active: {cpu_o}%\nTotal CPU Cores: {cpu_total}\nUsable CPU Cores: {cpu_usable}\nPhysical CPU Cores: {cpu_physical}\n-----------------------------------------------------------------\nCPU Frequency :-\nCurrent: {cpu_cur} mhz\nMAX: {cpu_max} mhz\nMIN: {cpu_min} mhz\n-----------------------------------------------------------------\nLoad Past 1 Min: {cpu_usage1}\nLoad Past 5 Min: {cpu_usage5}\nLoad Past 15 Min: {cpu_usage15}\n")
     return
