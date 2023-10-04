adf1e7790435290a9a623ed740fa2a4e0e1df3ec
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-04 17:29:30 +0530

    Update quick_info.py

--- main/plugins/quick_info.py
+++ main/plugins/quick_info.py
@@ -9,12 +9,12 @@ from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
 async def quick_info(event):
     cpu = str(psutil.cpu_percent()) + '%'
     memory = psutil.virtual_memory()
-    available = round(memory.available/1024.0/1024.0,2)
-    total = round(memory.total/1024.0/1024.0,2)
-    mem_info = str(available) + 'MB free / ' + str(total) + 'MB total ( ' + str(memory.percent) + '% )'
+    available = round(memory.available/1024.0/1024.0/1024.0,2)
+    total = round(memory.total/1024.0/1024.0/1024.0,2)
+    mem_info = str(available) + 'MB Free / ' + str(total) + 'MB Total ( ' + str(memory.percent) + '% )'
     disk = psutil.disk_usage('/')
     free = round(disk.free/1024.0/1024.0/1024.0,2)
     total = round(disk.total/1024.0/1024.0/1024.0,2)
-    disk_info = str(free) + 'GB free / ' + str(total) + 'GB total ( ' + str(disk.percent) + '% )'
-    await event.reply(f"CPU Info: {cpu}/nMemory Info: {mem_info}\nDisk Info: {disk_info}")
+    disk_info = str(free) + 'GB Free / ' + str(total) + 'GB Total ( ' + str(disk.percent) + '% )'
+    await event.reply(f"CPU Info: {cpu}\nMemory Info: {mem_info}\nDisk Info: {disk_info}")
     return
