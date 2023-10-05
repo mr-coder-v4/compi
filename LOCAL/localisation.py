1d99b99c4ddb6997ea8e1eff30f1b40f478d1df3
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-05 10:28:15 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -5,13 +5,13 @@ START_TEXT = "Send me any file to begin."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = ""
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = ""
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by High End Developer\n\nWritten in python library TELETHON"   
 
-help_text = """**v1.4**
+help_text = """**v2.1**
 
 •`Encode` - encode your video into different lib format or resolution
 
@@ -31,7 +31,7 @@ source_text = "**Deploy your own bot**"
 
 DEV = "https://t.me/MaheshChauhan"
 
-spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+spam_notice = "This bot is hosted on High End Cloud Server.Spamming the bot or encoding adult videos will lead you to a ban."
 
 JPG = "LOCAL/video_convertor.jpg"
 
