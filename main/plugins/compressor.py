bc82e5a93a15a381b1ef937bd4fe74d96efc3613
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-26 19:03:35 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -100,9 +100,9 @@ async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
     os.rename(out, out2)
     i_size = os.path.getsize(name)
     f_size = os.path.getsize(out2)
-    text = f'{msg.file.name} {wdt}×{hgt}'
+    text = f'{msg.file.name}'
     if ps_name != "**ENCODING:**":
-        text = f'{msg.file.name} {wdt}×{hgt}'
+        text = f'{msg.file.name}'
     UT = time.time()
     if 'webm' in mime:
         try:
