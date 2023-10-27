7ea865f9d6319674b6869a89973b90cd42859640
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-27 18:32:33 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -83,7 +83,7 @@ async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
     elif ffmpeg_cmd == 4:
         cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset faster -vcodec libx264 -crf 23 -acodec copy -c:s copy -map 0 """{out}""" -y'
     elif ffmpeg_cmd == 5:
-        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -pix_fmt yuv420p -vf scale=1280x720:flags=lanczos -acodec copy -b:a 128k -c:s copy -map 0 """{out}""" -y'
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -pix_fmt yuv420p -b:v 2500k -vf scale=1280x720:flags=lanczos -acodec copy -b:a 256k -c:s copy -map 0 """{out}""" -y'
     elif ffmpeg_cmd == 6:
         cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -pix_fmt yuv420p -vf scale=1920x1080:flags=lanczos -acodec copy -b:a 128k -c:s copy -map 0 """{out}""" -y'
     try:
