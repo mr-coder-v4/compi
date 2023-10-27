bc48d157b432e26eb10b0f06c6d934d2e5c0551d
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-27 16:17:34 +0530

    Update compressor.py

--- main/plugins/compressor.py
+++ main/plugins/compressor.py
@@ -83,9 +83,9 @@ async def compress(event, msg, ffmpeg_cmd=0, ps_name=None):
     elif ffmpeg_cmd == 4:
         cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset faster -vcodec libx264 -crf 23 -acodec copy -c:s copy -map 0 """{out}""" -y'
     elif ffmpeg_cmd == 5:
-        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265 -vf scale=1280x720:flags=lanczos -acodec copy -c:s copy -map 0 """{out}""" -y'
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -pix_fmt yuv420p -vcodec libx265 -vf scale=-1:720:flags=lanczos -acodec copy -c:s copy -map 0 """{out}""" -y'
     elif ffmpeg_cmd == 6:
-        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -vcodec libx265  -vf scale=-1:1440 -acodec copy -c:s copy -map 0 """{out}""" -y'
+        cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" -preset ultrafast -pix_fmt yuv420p -vcodec libx265  -vf scale=-1:1080:flags=lanczos -acodec copy -c:s copy -map 0 """{out}""" -y'
     try:
         await ffmpeg_progress(cmd, name, progress, FT, edit, ps_name)
     except Exception as e:
