dbf3b6600cad3bd32161b443d13498104a63a341
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-11-14 17:11:02 +0530

    Update new_cmd.py

--- main/plugins/new_cmd.py
+++ main/plugins/new_cmd.py
@@ -81,10 +81,8 @@ async def show_status(event):
     swap_p = swap.percent
     stt = dt.now()
     ed = dt.now()
-    v = ts(int((ed - uptime).seconds) * 1000)
     ms = (ed - stt).microseconds / 1000
-    p = f"🌋Pɪɴɢ = {ms}ms"
-    cp = v + "\n" + p
+    p = {ms}ms
     memory = virtual_memory()
     mem_t = humanbytes(memory.total)
     mem_a = humanbytes(memory.available)
@@ -92,7 +90,7 @@ async def show_status(event):
     await event.reply(f"""`
 Bot Uptime: {currentTime}
 OS: {osUptime}
-Ping: {cp}
+Ping: {p}
 UL: {sent} | DL: {recv}
 -------------------------
 Total Disk: {total}
