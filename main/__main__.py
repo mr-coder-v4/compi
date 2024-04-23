cd72983f033cd6b8dea6dae3be5ade749b82f9e5
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2024-04-23 14:46:18 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -16,7 +16,6 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Successfully deployed!")
-print("@MaheshChauhan • @DroneBots")
 
 if __name__ == "__main__":
     Drone.run_until_disconnected()
